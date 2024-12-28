e556c81f6e500c72a4e0c4f9d7b944e2bbd87074
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-28 23:42:30 +0600

    Badge Page

---
+++ src/components/common/HeaderPage/BadgePage.jsx
@@ -0,0 +1,67 @@
+import { Badge } from "@mui/material";
+import React from "react";
+import { CiHeart, CiRepeat } from "react-icons/ci";
+import { PiShoppingCartThin } from "react-icons/pi";
+import { LiaUserCircle } from "react-icons/lia";
+
+const BadgePage = () => {
+  return (
+    <div>
+      <div className="flex items-center gap-2 ml-3">
+        <div className="flex lg:hidden">
+          <LiaUserCircle 
+           size={30}
+              className="text-black hover:text-primary transition duration-1000"
+          />
+        </div>
+        <div className=" hidden md:block">
+          <Badge
+            badgeContent={4}
+            sx={{
+              "& .MuiBadge-badge": {
+                backgroundColor: "#ff5722", // Custom background color
+                color: "#fff", // Custom text color
+              },
+            }}
+          >
+            <CiRepeat
+              size={30}
+              className="text-black hover:text-primary transition duration-1000"
+            />
+          </Badge>
+        </div>
+
+        <Badge
+          badgeContent={4}
+          sx={{
+            "& .MuiBadge-badge": {
+              backgroundColor: "#ff5722", // Custom background color
+              color: "#fff", // Custom text color
+            },
+          }}
+        >
+          <CiHeart
+            size={30}
+            className="text-black hover:text-primary transition duration-1000"
+          />
+        </Badge>
+        <Badge
+          badgeContent={4}
+          sx={{
+            "& .MuiBadge-badge": {
+              backgroundColor: "#ff5722", // Custom background color
+              color: "#fff", // Custom text color
+            },
+          }}
+        >
+          <PiShoppingCartThin
+            size={30}
+            className="text-black hover:text-primary transition duration-1000"
+          />
+        </Badge>
+      </div>
+    </div>
+  );
+};
+
+export default BadgePage;
