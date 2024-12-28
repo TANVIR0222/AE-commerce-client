ba93c048a8b958fd367b3377a3896f3a3eb9468f
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-28 23:42:44 +0600

    Brand Page

---
+++ src/components/common/HeaderPage/BrandPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const BrandPage = () => {
+  return (
+    <div className=" space-y-3">
+      <p className="fashon_deshing texthover">Gadget Zone</p>
+      <p className="fashon_deshing texthover">Initech Space</p>
+      <p className="fashon_deshing texthover">Looney Tunes</p>
+      <p className="fashon_deshing texthover">Massive Dynamic</p>
+      <p className="fashon_deshing texthover">Pro Tech Gear</p>
+      <p className="fashon_deshing texthover">Soylent Green</p>
+      <p className="fashon_deshing texthover">The Simpsons</p>
+      <p className="fashon_deshing texthover">Weeds Capital</p>
+    </div>
+  );
+};
+
+export default BrandPage;
