4e1084b3ff938d600aa044e2f1d70097362c1c15
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-28 22:47:35 +0600

    all  data

---
+++ src/utils/data.js
@@ -0,0 +1,119 @@
+export const subHeader = [
+    {
+        id: 1,
+        name: "Help Center"
+    },
+    {
+        id: 2,
+        name: "Order Tracking"
+    },
+    {
+        id: 3,
+        name: "Bangladesh"
+    },
+    {
+        id: 4,
+        name: "BDT"
+    },
+]
+
+export const countryData = [
+    { name: "Bangladesh", code: "BD", flag: "🇧🇩" },
+    { name: "United States", code: "US", flag: "🇺🇸" },
+    { name: "Canada", code: "CA", flag: "🇨🇦" },
+    { name: "United Kingdom", code: "GB", flag: "🇬🇧" },
+    { name: "Germany", code: "DE", flag: "🇩🇪" },
+  ];
+
+  export const navbar = [
+    {
+        name: "Home",
+        link: "/",
+
+    },
+    {
+        name: "Fashion",
+        link: "/",
+
+    },
+    {
+        name: "New Arrivals",
+        link: "/",
+
+    },
+    {
+        name: "All brands",
+        link: "/",
+
+    },
+    {
+        name: "Contact",
+        link: "/",
+
+    },
+];
+
+export const slides = [
+    {
+      id: 2,
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_imageslider/views/img/sample-2.jpg",
+      alt: "Slide 2",
+      subTitle: "Big Saving Days Sale",
+      title: "Buy Modern Chair In Black Color",
+      description: "Starting At Only",
+      price: " $ 99.99",
+    },
+    {
+      id: 3,
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_imageslider/views/img/sample-1.jpg",
+      alt: "Slide 3",
+      subTitle: "Big Saving Days Sale",
+      title: "Women Solid Round Green T-Shirt",
+      description: "Starting At Only",
+      price: " $ 59.99",
+    },
+];
+
+
+export const minicart = [
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/11-cz_categoryimagelist.jpg",
+      title: "Smart Tablet",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/12-cz_categoryimagelist.jpg",
+      title: "Crepe T-Shirt",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/13-cz_categoryimagelist.jpg",
+      title: "leather watch",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/14-cz_categoryimagelist.jpg",
+      title: "Rolling Diamond",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/15-cz_categoryimagelist.jpg",
+      title: "Wooden Chair",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/16-cz_categoryimagelist.jpg",
+      title: "Sneakers Shoes",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/17-cz_categoryimagelist.jpg",
+      title: "Purse",
+      
+    },
+    {
+      url: "https://demos.codezeel.com/prestashop/PRS21/PRS210502/modules/cz_categoryimagelist/views/img/18-cz_categoryimagelist.jpg",
+      title: "Xbox Controller",
+      
+    },
+];
