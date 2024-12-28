b1be3031838b7b31752e910ea0588b04b8aed732
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-28 23:44:36 +0600

    Free Shipping page

---
+++ src/components/pages/Home/FreeShipping.jsx
@@ -0,0 +1,27 @@
+import { FaShippingFast } from "react-icons/fa";
+
+const FreeShipping = () => {
+  return (
+    <div className="container ">
+      <div className=" grid grid-cols-1 text-center  md:flex justify-between items-center border-2 border-primary my-10 md:my-16 p-6">
+        <div className=" flex items-center gap-2 text-center justify-center">
+            <FaShippingFast size={40} className=" hidden md:block" />
+          <h2 className="text-[30px] font-medium text-textPrimary">Free Shipping</h2>
+        </div>
+        <span className="p-3 border-r border-gray-300"></span>
+
+        <div className="">
+          <p className="text-[20px] font-light text-textPrimary">
+            Get free shipping on all orders over $
+            <span className="text-primary">200</span>
+          </p>
+        </div>
+        <span className="p-3 border-r border-gray-300"></span>
+
+        <div className="text-textPrimary font-semibold text-[30px]">- Only $200*</div>
+      </div>
+    </div>
+  );
+};
+
+export default FreeShipping;
