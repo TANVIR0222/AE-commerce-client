ee2cbe4284979e65584addd66709675f7cba2ced
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-30 15:17:47 +0600

    Free Shipping page responsive size add

--- src/components/pages/Home/FreeShipping.jsx
+++ src/components/pages/Home/FreeShipping.jsx
@@ -3,12 +3,12 @@ import { FaShippingFast } from "react-icons/fa";
 const FreeShipping = () => {
   return (
     <div className="container ">
-      <div className=" grid grid-cols-1 text-center  md:flex justify-between items-center border-2 border-primary my-10 md:my-16 p-6">
+      <div className=" grid grid-cols-1 md:grid-cols-1 text-center  lg:flex justify-between items-center border-2 border-primary my-10 md:my-16 p-6">
         <div className=" flex items-center gap-2 text-center justify-center">
-            <FaShippingFast size={40} className=" hidden md:block" />
+            <FaShippingFast size={40} className=" hidden md:hidden lg:block" />
           <h2 className="text-[30px] font-medium text-textPrimary">Free Shipping</h2>
         </div>
-        <span className="p-3 border-r border-gray-300"></span>
+        <span className="p-3 border-r border-gray-300 hidden md:hidden lg:block"></span>
 
         <div className="">
           <p className="text-[20px] font-light text-textPrimary">
@@ -16,7 +16,7 @@ const FreeShipping = () => {
             <span className="text-primary">200</span>
           </p>
         </div>
-        <span className="p-3 border-r border-gray-300"></span>
+        <span className="p-3 border-r border-gray-300 hidden md:hidden lg:block"></span>
 
         <div className="text-textPrimary font-semibold text-[30px]">- Only $200*</div>
       </div>
