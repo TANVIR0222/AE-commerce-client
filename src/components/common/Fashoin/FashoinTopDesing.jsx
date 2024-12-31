90cf3ec385eab9cfe333df8c8ddb61323c7040d7
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-01 00:00:27 +0600

    Fashoin Top Desing  page

---
+++ src/components/common/Fashoin/FashoinTopDesing.jsx
@@ -0,0 +1,22 @@
+const FashoinTopDesing = () => {
+  return (
+    <div>
+      <img
+        className=" rounded-md"
+        src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/c/3-category_default/clothes.jpg"
+        alt=""
+      />
+      <div className=" space-y-3 my-7">
+        <h1 className="text-[22px] font-medium">Fashion</h1>
+        <p className="text-[14px] text-textPrimary font-normal ">
+          Fashion has been creating well-designed collections since 2010. The
+          brand offers feminine designs delivering stylish separates and
+          statement dresses which has since evolved.
+        </p>
+      </div>
+      <hr />
+    </div>
+  );
+};
+
+export default FashoinTopDesing;
