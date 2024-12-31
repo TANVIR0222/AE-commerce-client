4101ec7a1a117e20cc5388cc07f44df7bdb706b6
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-12-31 23:59:42 +0600

    Description Page

---
+++ src/components/common/Fashoin/DescriptionPage.jsx
@@ -0,0 +1,17 @@
+
+
+const DescriptionPage = () => {
+    return (
+        <div className="h-[350px] border">
+            <div className="p-8 space-y-3">
+                <p className=" text-[14px] text-textSecondary w-[1078px] leading-7 ">Studio Design' PolyFaune collection features classic products with colorful patterns, inspired by the traditional japanese origamis. To wear with a chino or jeans. The sublimation textile printing process provides an exceptional color rendering and a color, guaranteed overtime.</p>
+                <h6 className="text-[14px] font-normal">Packaging & Delivery</h6>
+                <p className=" text-[14px] text-textSecondary w-[1078px] leading-7 ">Less lion goodness that euphemistically robin expeditiously bluebird smugly scratched far while thus cackled sheepishly rigid after due one assenting regarding censorious while occasional or this more crane went more as this less much amid overhung anathematic because much held one exuberantly sheep goodness so where rat wry well concomitantly.</p>
+                <h6  className="text-[14px] font-normal">Suggested Use</h6>
+                <p  className=" text-[14px] text-textSecondary w-[1078px] leading-7 ">Scallop or far crud plain remarkably far by thus far iguana lewd precociously and and less rattlesnake contrary caustic wow this near alas and next and pled the yikes articulate about as less cackled dalmatian in much less well jeering for the thanks blindly sentimental whimpered less across objectively fanciful grimaced wildly some wow and rose jeepers outgrew lugubrious luridly irrationally attractively dachshund.</p>
+            </div>
+        </div>
+    );
+};
+
+export default DescriptionPage;
