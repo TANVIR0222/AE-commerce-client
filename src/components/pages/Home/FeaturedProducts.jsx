264aa4444ac87aeaf092f93f62b01e46fd4108f4
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-01 00:02:51 +0600

    Featured Products page

--- src/components/pages/Home/FeaturedProducts.jsx
+++ src/components/pages/Home/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
 import { productsData } from "../../../utils/data";
 import { useEffect, useState } from "react";
+import FashionCart from "../Fashoin/FashionCart";
 
 const FeaturedProducts = () => {
   const [visibleItems, setVisibleItems] = useState(6);
@@ -80,23 +81,7 @@ const FeaturedProducts = () => {
                         : "w-1/2" // 2 items on small screens
                     }`}
                   >
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="w-full h-52 my-2 rounded-md p-1"
-                    />
-                    <div className="ml-4 space-y-2">
-                      <p className="text-[13px] font-light text-textSecondary">
-                        {item.category}
-                      </p>
-                      <p className="text-[14px] font-normal text-black">
-                        {item.description}
-                      </p>
-                      <p className="text-[13px]">{item.rating}</p>
-                      <p className="text-[15px] text-primary font-normal">
-                        ${item.price}
-                      </p>
-                    </div>
+                    <FashionCart item={item} />
                   </div>
                 ))}
               </div>
