e61c581da1f02480685a96979bfef11c6629a282
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2025-01-01 23:10:39 +0600

    isScrolled hidden

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,22 +1,44 @@
+import { Link } from "react-router";
 import BadgePage from "../components/common/HeaderPage/BadgePage";
 import HeaderPage from "../components/common/HeaderPage/HeaderPage";
 import MiniBannerFlag from "../components/common/HeaderPage/MiniBannerFlag";
 import SearchPage from "../components/common/HeaderPage/SearchPage";
 import SidebarPage from "../components/common/HeaderPage/SideBarPage";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="">
+    <header className="sticky top-0 z-40 bg-white">
       {/* Top mini Header */}
-      <div className="top-script">
+      <div className={`top-script  ${isScrolled? " hidden duration-1000 shadow-md" : "bg-transparent  text-white"}`}>
         <div className="container py-2">
-          <div className=" flex items-center justify-between">
+          <div className={`flex items-center justify-between`}>
             <div className="text-[14px]">
               <p className=" text-textSecondary transition-all hidden md:hidden lg:block">
                 Get up to 50% off new season styles, limited time only
               </p>
             </div>
-            <div className=" flex items-center gap-2 mx-auto md:mx-auto lg:mx-1 ">
+            <div className={` flex items-center gap-2 mx-auto md:mx-auto lg:mx-1 
+               `}>
               <MiniBannerFlag />
             </div>
           </div>
@@ -24,40 +46,47 @@ const Header = () => {
       </div>
 
       {/* Mid mini Header */}
-      <div className="tb-script">
+      <div className={`tb-script ${isScrolled ? "block lg:hidden shadow-md": "bg-transparent text-white"}`}>
         <div className="container">
           {/*  */}
-          <div className="flex items-center justify-between my-5">
+          <div className={`flex items-center justify-between my-5` }>
             <div className="flex items-center gap-2">
-              <div className=" flex md:hidden">
+              <div className=" flex lg:hidden">
                 <SidebarPage />
               </div>
-              <img
-                src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/logo-1691412328.jpg"
-                alt=""
-                className="w-32 lg:w-[150px] h-[30px]"
-              />
+              <Link to={'/'} >
+                <img
+                  src="https://demos.codezeel.com/prestashop/PRS21/PRS210502/img/logo-1691412328.jpg"
+                  alt=""
+                  className="w-32 lg:w-[150px] h-[30px]"
+                />
+              </Link>
+              
             </div>
             {/* search page  */}
-            <div className=" hidden md:block lg:block">
+            <div className="  hidden md:block lg:block">
               <SearchPage />
             </div>
             {/* auth */}
             <div className="flex items-center">
               <div className=" hidden lg:block">
                 <div className="flex items-center gap-1">
+                  <Link to="/login">
                   <p className="text-[14px] text-black font-light hover:text-primary transition duration-1000">
                     Login{" "}
                   </p>
+                  </Link>
                   /
+                  <Link to={'/register'}>
                   <p className="text-[14px] text-textPrimary font-light hover:text-primary transition duration-1000">
                     Registe
                   </p>
+                  </Link>
                   <span className="p-3 border-r border-gray-300"></span>
                 </div>
               </div>
               {/* user badges */}
-              <div className="flex items-center gap-2 ml-3">
+              <div className={`flex items-center gap-2 ml-3 sticky top-0 z-40  `}>
                 <BadgePage />
               </div>
             </div>
@@ -66,13 +95,13 @@ const Header = () => {
       </div>
       {/* HeaderPage */}
       <div className="tb-script">
-        <div className="container hidden lg:block">
+        <div className={ `container hidden lg:block`}>
           {/*  */}
           <HeaderPage />
         </div>
       </div>
       {/* mobile  */}
-      <div className="top-script flex md:hidden lg:hidden ">
+      <div className={`top-script flex md:hidden lg:hidden ${isScrolled ? " hidden shadow-md" : "bg-transparent opacity-55 text-white"} `}>
         <div className="my-2 ">
           <SearchPage />
           <hr />
